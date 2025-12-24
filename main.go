13effa65dc3189d62eabb2a35f212b28d98202c6
Kaue Silveira <silveira.kaue@gmail.com>
2025-12-24 19:14:29 +0100

    white pawn move

--- main.go
+++ main.go
@@ -6,6 +6,17 @@ import (
 	"strings"
 )
 
+type Point struct {
+	X int
+	Y int
+}
+
+type Move struct {
+	What string
+	From Point
+	To   Point
+}
+
 func main() {
 	board := []string{
 		"bnrk",
@@ -14,4 +25,19 @@ func main() {
 		"KRNB",
 	}
 	fmt.Println(strings.Join(board, "\n"))
+	moves := []Move{}
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if board[i][j] == 'P' {
+				ni := i - 1
+				nj := j
+				if board[ni][nj] == ' ' {
+					moves = append(moves, Move{What: string(board[i][j]), From: Point{X: i, Y: j}, To: Point{X: ni, Y: nj}})
+				}
+			}
+		}
+	}
+	for _, move := range moves {
+		fmt.Println(move)
+	}
 }

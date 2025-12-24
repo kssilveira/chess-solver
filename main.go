872bac4f1919125a7df830ca97525669fc813b96
Kaue Silveira <silveira.kaue@gmail.com>
2025-12-24 19:02:10 +0100

    starting board

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 // Package main solves tinyhouse chess variant.
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
+	board := []string{
+		"bnrk",
+		"   p",
+		"P   ",
+		"KRNB",
+	}
+	fmt.Println(strings.Join(board, "\n"))
 }

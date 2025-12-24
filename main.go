c4421cab3c72a58ec1d2a9c9169c4171baa21858
Kaue Silveira <silveira.kaue@gmail.com>
2025-12-24 19:27:53 +0100

    execute white pawn move

--- main.go
+++ main.go
@@ -2,29 +2,24 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"strings"
 )
 
 type Point struct {
-	X int
-	Y int
+	X    int
+	Y    int
+	What byte
 }
 
 type Move struct {
-	What string
 	From Point
 	To   Point
 }
 
-func main() {
-	board := []string{
-		"bnrk",
-		"   p",
-		"P   ",
-		"KRNB",
-	}
-	fmt.Println(strings.Join(board, "\n"))
+func solve(board [][]byte, depth int) {
+	fmt.Printf("\ndepth: %d\n", depth)
+	fmt.Println(string(bytes.Join(board, []byte("\n"))))
 	moves := []Move{}
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
@@ -32,12 +27,28 @@ func main() {
 				ni := i - 1
 				nj := j
 				if board[ni][nj] == ' ' {
-					moves = append(moves, Move{What: string(board[i][j]), From: Point{X: i, Y: j}, To: Point{X: ni, Y: nj}})
+					moves = append(moves, Move{
+						From: Point{What: board[i][j], X: i, Y: j},
+						To:   Point{What: board[ni][nj], X: ni, Y: nj}})
 				}
 			}
 		}
 	}
 	for _, move := range moves {
-		fmt.Println(move)
+		board[move.To.X][move.To.Y] = board[move.From.X][move.From.Y]
+		board[move.From.X][move.From.Y] = ' '
+		solve(board, depth+1)
+		board[move.To.X][move.To.Y] = move.To.What
+		board[move.From.X][move.From.Y] = move.From.What
 	}
+}
+
+func main() {
+	board := [][]byte{
+		[]byte("bnrk"),
+		[]byte("   p"),
+		[]byte("P   "),
+		[]byte("KRNB"),
+	}
+	solve(board, 0 /* depth */)
 }
